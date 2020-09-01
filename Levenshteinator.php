2ac65a9578d52e959937a4e8c0e974e7252b44f5
Adrian <adrian-enspired@users.noreply.github.com>
2020-09-01 11:38:08 -0400

    Update Levenshteinator.php

--- Levenshteinator.php
+++ Levenshteinator.php
@@ -4,17 +4,6 @@
  * @author     Adrian <adrian@enspi.red>
  * @copyright  2017
  * @license    no license, no warranty
- *
- *  This program is free software: you can redistribute it and/or modify it
- *  under the terms of the GNU General Public License, version 3.
- *  The right to apply the terms of later versions of the GPL is RESERVED.
- *
- *  This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
- *  without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
- *  See the GNU General Public License for more details.
- *
- *  You should have received a copy of the GNU General Public License along with this program.
- *  If not, see <http://www.gnu.org/licenses/gpl-3.0.txt>.
  */
 declare(strict_types = 1);
 
@@ -23,8 +12,8 @@ namespace at\levenshteinable;
 use at\levenshteinable\Levenshteinable;
 
 /**
- * forgot how to spell the name of that class?
- * just type in something close, you're golden!
+ * Forgot how to spell the name of that class?
+ * Just type in something close, you're golden!
  */
 class Levenshteinator {
 
